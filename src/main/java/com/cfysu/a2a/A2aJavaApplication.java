66a9cb8d2dac3d5a0069463d27f87712bff29e44
canglong <cangjian.lcj@alibaba-inc.com>
2025-05-23 17:56:25 +0800

    feat(A2aJavaApplication): 添加新应用及其配置和测试
    添加了一个新的Java应用程序A2aJavaApplication，包括主程序文件、配置文件application.properties以及对应的单元测试文件A2aJavaApplicationTests。

---
+++ src/main/java/com/cfysu/a2a/A2aJavaApplication.java
@@ -0,0 +1,13 @@
+package com.cfysu.a2a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A2aJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A2aJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cfysu/a2a/A2aJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfysu.a2a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A2aJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
